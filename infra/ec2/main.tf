d2598f53140f49430f78ea068eead889e9975de9
daresamuel0000 <daresamuel0000@gmail.com>
2025-06-23 01:44:30 +0100

    Update main.tf

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -9,7 +9,7 @@ variable "user_data_install_apache" {}
 variable "ec2_sg_name_for_python_api" {}
 
 output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "C:\Users\dares\Downloads\my-cba-project-bucket-2.pem", aws_instance.jenkins_ec2_instance_ip.public_ip)
+  value = format("%s%s", "C:/Users/dares/Downloads/my-cba-project-bucket-2.pem", aws_instance.jenkins_ec2_instance_ip.public_ip)
 }
 
 output "dev_proj_1_ec2_instance_id" {
